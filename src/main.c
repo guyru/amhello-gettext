a6aa8a48c99b9c4e1d2fb2c8d9810e639cc9967c
Guy Rutenberg <guyrutenberg@gmail.com>
2014-10-17 14:47:07 +0300

    Trigger `gettext` in main().

--- src/main.c
+++ src/main.c
@@ -1,9 +1,14 @@
 #include <config.h>
 #include <stdio.h>
+#include "gettext.h"
 
 int
 main (void)
 {
+  setlocale (LC_ALL, "");
+  bindtextdomain (PACKAGE, LOCALEDIR);
+  textdomain (PACKAGE);
+
   puts ("Hello World!");
   return 0;
 }
